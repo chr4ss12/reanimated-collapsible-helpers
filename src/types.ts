aac5ac034c472e6e0a60926fea8da2964ef1fa18
chriseelmaa <chriseelmaa@gmail.com>
2024-02-16 14:58:38 +0000

    fix

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,10 @@
-import { EasingFunction } from "react-native/Libraries/Animated/Easing";
 import { SharedValue } from "react-native-reanimated";
 
 export declare type State = "expanded" | "collapsed";
 
 export declare type Config = {
   duration?: number;
-  easing?: EasingFunction;
+  easing?: any;
   mounted?: SharedValue<boolean>;
   show?: boolean | null | undefined;
   state?: string;
